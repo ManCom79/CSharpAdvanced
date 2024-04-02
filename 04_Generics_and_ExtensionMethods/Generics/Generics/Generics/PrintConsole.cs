24cd3873b6fd96439a17541fa2fcda479cbca151
Sasha Stamenkovski <prof.menko@gmail.com>
2024-04-02 22:08:03 +0200

    Generic methods

---
+++ 04_Generics_and_ExtensionMethods/Generics/Generics/Generics/PrintConsole.cs
@@ -0,0 +1,19 @@
+﻿namespace Generics
+{
+    internal class PrintConsole
+    {
+        public static void Print<T>(T item)
+        {
+            Console.WriteLine($"The item passed to be printed is \"{item}\"");
+        }
+
+        public static void PrintCollection<T>(T[] items)
+        {
+            foreach(T item in items)
+            {
+                Console.WriteLine($"My name is \"{item}\" and I am just a single entry in the array.");
+            }
+        }
+
+    }
+}
---
+++ 04_Generics_and_ExtensionMethods/Generics/Generics/Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            double doubleNumber = 2.0;
+            char character = 'a';
+            string words = "Generic String";
+
+            int[] integers = { 1, 2, 3, 4, 5 };
+            string[] integerStrigs = { "one", "two", "three", "four", "five" };
+
+            PrintConsole.Print(number);
+            PrintConsole.Print(doubleNumber);
+            PrintConsole.Print(character);
+            PrintConsole.Print(words);
+
+            PrintConsole.PrintCollection(integers);
+            PrintConsole.PrintCollection(integerStrigs);
+            
+        }
+    }
+}
