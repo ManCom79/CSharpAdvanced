699d9fa3b24480a332bd5a4a67b6ae682ccd18cc
Sasha Stamenkovski <prof.menko@gmail.com>
2024-04-19 08:14:10 +0200

    Writing names to file solved

--- 09_RW_File_System/ReadWrite_to_File_System/ReadWrite_to_File_System/Program.cs
+++ 09_RW_File_System/ReadWrite_to_File_System/ReadWrite_to_File_System/Program.cs
@@ -16,7 +16,34 @@
             if (!File.Exists(filePath))
             {
                 Console.WriteLine($@"Creating file names.txt at {filePath}");
-                File.Create(filePath);
+                var file = File.Create(filePath);
+                file.Close();
+
+            }
+
+            using (var streamReader =  new StreamReader(filePath))
+            {
+                var fileNamesContent = streamReader.ReadToEnd();
+            }
+
+            List<string> names = new List<string>();
+            int count = 0;
+
+            while (count < 3)
+            {
+                Console.WriteLine("Please enter a name");
+                string name = Console.ReadLine();
+                names.Add(name);
+                count++;
+            }
+
+            using (var streamWrite = new StreamWriter(filePath))
+            {
+                foreach (string name in names)
+                {
+                    streamWrite.WriteLine(name);
+                }
+                Console.WriteLine($"All names are written at {folderPath}.");
             }
         }
     }
