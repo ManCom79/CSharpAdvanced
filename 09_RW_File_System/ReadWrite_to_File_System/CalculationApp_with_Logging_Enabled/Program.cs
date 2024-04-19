546ac1c79fb8057f771ecaaa3576dafcc1d6ca95
Sasha Stamenkovski <prof.menko@gmail.com>
2024-04-20 00:03:50 +0200

    Task 5 solved: logging included

--- 09_RW_File_System/ReadWrite_to_File_System/CalculationApp_with_Logging_Enabled/Program.cs
+++ 09_RW_File_System/ReadWrite_to_File_System/CalculationApp_with_Logging_Enabled/Program.cs
@@ -1,15 +1,23 @@
-﻿namespace CalculationApp_with_Logging_Enabled
+﻿using Serilog;
+
+namespace CalculationApp_with_Logging_Enabled
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             string folderPath = @"C:\Exercise";
             string filePath = $@"{folderPath}\calculations.txt";
 
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.File(@"C:\Exercise\logs-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
             if (!Directory.Exists(folderPath))
             {
                 Directory.CreateDirectory(@"C:\Exercise");
+                Log.Information(@"Folder ""C:\Exercise"" created.");
             }
 
             if (!File.Exists(filePath))
@@ -25,24 +33,30 @@
             {
                 Console.WriteLine($"Call {counter}:");
                 Console.Write("Please enter the first number: ");
+                Log.Information("Entering the first number!");
                 string number1 = Console.ReadLine();
 
                 if (!int.TryParse(number1, out sumNumber1))
                 {
                     Console.WriteLine("The entered value was not numeric!");
+                    Log.Error("The entered value was not numeric!");
                     continue;
                 }
+                Log.Information($"First logged number is {sumNumber1}");
 
                 while (true)
                 {
                     Console.Write("Please enter the second number: ");
+                    Log.Information("Entering the second number!");
                     string number2 = Console.ReadLine();
 
                     if (!int.TryParse(number2, out sumNumber2))
                     {
                         Console.WriteLine("The entered value was not numeric!");
+                        Log.Error("The entered value was not numeric!");
                         continue;
                     }
+                    Log.Information($"Second logged number is {sumNumber1}");
                     break;
                 }
 
@@ -53,6 +67,7 @@
                 using(var inFileCalculation = new StreamWriter(filePath, true))
                 {
                     inFileCalculation.WriteLine($"[{DateTime.Now}] {resultOfCalculation}");
+                    Log.Information($"Written to file is: [{DateTime.Now}] {resultOfCalculation}");
                 }
 
                 counter++;
@@ -61,6 +76,7 @@
 
         public static string CalulateSum(int a, int b)
         {
+            Log.Information("Function 'CalculateSum is calculating...'");
             int result = a + b;
             return $"{a} + {b} = {result}";
         }
