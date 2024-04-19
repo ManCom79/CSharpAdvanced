c4f9ce84fba49280cbb990917ac3074e96b930c0
Sasha Stamenkovski <prof.menko@gmail.com>
2024-04-19 07:58:39 +0200

    Creating folder and file on fs

---
+++ 09_RW_File_System/ReadWrite_to_File_System/ReadWrite_to_File_System/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReadWrite_to_File_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"C:\Files";
+            string filePath = $@"{folderPath}\names.txt";
+
+            if (!Directory.Exists(folderPath))
+            {
+                Console.WriteLine(@"Creating folder C:\Files");
+                Directory.CreateDirectory(folderPath);
+            }
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($@"Creating file names.txt at {filePath}");
+                File.Create(filePath);
+            }
+        }
+    }
+}
